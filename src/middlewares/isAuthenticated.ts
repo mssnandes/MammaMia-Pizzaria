c08bb5e39de269c0d9e2fae7c88ae738833b9801
Moises <moisesfens@gmail.com>
2025-09-25 16:44:03 -0300

    finalizando middeware e rota /me para trazer informacoes do usuario logado com base no token

--- src/middlewares/isAuthenticated.ts
+++ src/middlewares/isAuthenticated.ts
@@ -2,7 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import { verify } from "jsonwebtoken";
 
 interface PayLoad{
-    sub: String;
+    sub: string;
 }
 
 export function isAuthenticated(
@@ -13,19 +13,17 @@ export function isAuthenticated(
     //Receber o token
     const authToken = req.headers.authorization;
 
-    if(!authToken){
-        return res.status(401).end();
-    }
+    if(!authToken) res.status(401).end();
 
     const [, token] = authToken.split(" ");
-
-    try {
-        const { sub } = verify(
-            token,
-            process.env.JWT_SECRET
-        ) as PayLoad;
+    
+    try{
+        const { sub } = verify( token, process.env.JWT_SECRET ) as PayLoad;
         
-    } catch (error) {
-        return res.status(401).end
+        req.user_id = sub
+
+        return next();
+    } catch(error){
+        res.status(401).end();
     }
 }
